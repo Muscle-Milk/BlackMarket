fa9d6d26877874bd35155fec46d447efa0ba37c4
Muscle-Milk <50158516+Muscle-Milk@users.noreply.github.com>
2019-04-30 19:58:59 -0500

    Add files via upload

---
+++ Black_Market/config.lua
@@ -0,0 +1,24 @@
+Config               = {}
+
+Config.DrawDistance  = 100
+Config.Size          = { x = 1.5, y = 1.5, z = 0.5 }
+Config.Color         = { r = 0, g = 128, b = 255 }
+Config.Type          = 1
+
+Config.Locale        = 'en'
+
+Config.LicenseEnable = false -- only turn this on if you are using esx_license
+Config.LicensePrice  = 5000
+
+Config.Zones = {
+
+
+	BlackWeashop = {
+		Legal = false,
+		Items = {},
+		Locations = {
+			vector3(184.37, -1513.55, 29.14)
+		}
+	}
+
+}
